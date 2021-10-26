8f1b8ecad780032472c2e92080f251b3b56ec1e1
saracin28 <robert.saracin@student.upt.ro>
2021-10-27 00:42:10 +0300

    assignment 7 completed

---
+++ Assignment7/Assignment7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assignment7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student> { 
+                new Student("John", "Smith", 21, "USA"), 
+                new Student("Maria", "Garcia", 23, "Spain"),
+                new Student("Bjorn", "Bjornsson", 21, "Norway"),
+                new Student("Xin","Lin",20,"China"),
+                new Student("Frigga", "Ragnarsdatter",22,"Norway"),
+                new Student("Francois", "Monet",23,"France"),
+                new Student("Rami", "Reddy",21,"India")
+            };
+
+            var filterByCountry = students.Where(x => x.Country.Equals("Norway"));
+
+            foreach (var f in filterByCountry)
+            {
+                Console.WriteLine(f.ToString());
+            }
+
+            var filterByAge = from s in students orderby s.Age descending group s.LastName by s.Age; 
+
+            foreach(var f in filterByAge)
+            {
+                Console.WriteLine(String.Format("Age: {0,-10}Count: {1}",f.Key,f.Count()));
+                foreach(var i in f)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ Assignment7/Assignment7/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+
+        public Student(string firstName, string lastName, int age, string country)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Country = country;
+        }
+
+        public string ToString()
+        {
+            return $"First Name: {FirstName},\nLast Name: {LastName},\nAge: {Age},\nCountry: {Country}\n";
+        }
+    }
+}

fca2f38ef1ed6ea63e991215a7b6ff1636eecd3a
saracin28 <robert.saracin@student.upt.ro>
2021-10-28 23:20:44 +0300

    assignment 8 completed

---
+++ Assignment8/Assignment8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Assignment8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo downolads = new DirectoryInfo(@"C:\Users\rober\Downloads");
+
+            FileInfo[] files = downolads.GetFiles();
+            var filtered = files.OrderBy(x=>x.CreationTime).GroupBy(x => x.CreationTime.ToString("dd-MM-yyyy"));
+
+            foreach (var group in filtered) 
+            {
+                foreach (var f in group)
+                {
+                    var newDir = Directory.CreateDirectory(@"C:\Users\rober\Documents\Downloads\"+ f.CreationTime.ToString("dd-MM-yyyy"));
+                    f.MoveTo(newDir.FullName + @"\" +f.Name);
+                }
+            }
+        }
+    }
+}
